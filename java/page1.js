dad055d86ce21abb03cb427bf5d07dd4894cd3d9
kek388 <32974966+kek388@users.noreply.github.com>
2017-11-22 21:01:55 +0800

    1

---
+++ java/page1.js
@@ -0,0 +1,226 @@
+jQuery(function ($) {
+
+var $window = $(window),
+    $w1 = $('.w1');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $w1.removeClass("w11");
+            $w1.addClass("w1");
+
+    } else if ($window.width() < 368.8) {
+            $w1.addClass("w11");
+            $w1.removeClass("w1");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $w2 = $('.w2');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $w2.removeClass("w21");
+            $w2.addClass("w2");
+
+    } else if ($window.width() < 368.8) {
+            $w2.addClass("w21");
+            $w2.removeClass("w2");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $w3 = $('.w3');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $w3.removeClass("w31");
+            $w3.addClass("w3");
+
+    } else if ($window.width() < 368.8) {
+            $w3.addClass("w31");
+            $w3.removeClass("w3");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $w4 = $('.w4');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $w4.removeClass("w41");
+            $w4.addClass("w4");
+
+    } else if ($window.width() < 368.8) {
+            $w4.addClass("w41");
+            $w4.removeClass("w4");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $w5 = $('.w5');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $w5.removeClass("w51");
+            $w5.addClass("w5");
+
+    } else if ($window.width() < 368.8) {
+            $w5.addClass("w51");
+            $w5.removeClass("w5");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $w6 = $('.w6');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $w6.removeClass("w61");
+            $w6.addClass("w6");
+
+    } else if ($window.width() < 368.8) {
+            $w6.addClass("w61");
+            $w6.removeClass("w6");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $w7 = $('.w7');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $w7.removeClass("w71");
+            $w7.addClass("w7");
+
+    } else if ($window.width() < 368.8) {
+            $w7.addClass("w71");
+            $w7.removeClass("w7");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $svgc = $('.svgc');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $svgc.removeClass("svgc1");
+            $svgc.addClass("svgc");
+
+    } else if ($window.width() < 368.8) {
+            $svgc.addClass("svgc1");
+            $svgc.removeClass("svgc");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $profitword = $('.profitword');
+
+
+function resize() {
+    if ($window.width() > 368.8) {
+            $profitword.removeClass("profitword");
+            $profitword.addClass("profitword1");
+
+    } else if ($window.width() < 368.8) {
+            $profitword.addClass("profitword");
+            $profitword.removeClass("profitword1");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
+
+
+
+$('.count1').each(function() {
+  $(this).prop('Counter', 0).animate({
+    Counter: $(this).text()
+  }, {
+    duration: 8388,
+    easing: 'swing',
+    step: function(now) {
+      $(this).text(Math.ceil(now));
+    }
+  });
+});
+
+jQuery(function ($) {
+
+var $window = $(window),
+    $pw = $('.pw');
+
+
+function resize() {
+    if ($window.width() > 888.8) {
+            $pw.removeClass("pw1");
+            $pw.addClass("pw");
+
+    } else if ($window.width() < 888.8) {
+            $pw.addClass("pw1");
+            $pw.removeClass("pw");}
+        
+ }
+
+ $window.resize(resize)
+        .trigger('resize');
+});
+
